69c4ed73867c8ff0b66c75cd625d777cca5aeea1
Vojislav Grujic <vojislav.grujic@gmail.com>
2019-06-22 13:39:47 +0200

    README file update

--- src/config/tmdb.js
+++ src/config/tmdb.js
@@ -1,5 +1,4 @@
 export default {
-    api_key: '38535774dcac925adbeee9476d08b67d',
     api_url_v3: 'https://api.themoviedb.org/3',
     auth_request_token_url: 'https://www.themoviedb.org/authenticate'
 }
